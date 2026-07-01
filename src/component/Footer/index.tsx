a4a0ffc6be1c36b55c3fd6b69e99c8746225ebc0
First Commit - 8 Januari 2025 <>
2026-07-01 16:19:10 +0800

    Update Footer

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
         <p className="text-white mt-1 text-sm md:text-base flex items-center justify-center space-x-1">
             <span>&copy;</span>
-            <span>2025 Diskominfo HST</span>
+            <span>DISKOMINFOSP HST</span>
             <span className="w-1 h-1 bg-white rounded-full"></span>
             <span>si-Dinda</span>
         </p>
